d270a7fa9dd6e56122e99ed7ffdde5db5059ed88
Michael W. Kearney <mike.wayne.kearney@gmail.com>
2022-01-05 15:13:46 -0600

    Update __init__.py

--- python/pytweet/requests/__init__.py
+++ python/pytweet/requests/__init__.py
@@ -7,13 +7,13 @@ def api_path(endpoint: str, params:dict=None)->str:
     args = "&".join(args)
     return f"{_base_url_}{endpoint}{args}"
 
-def api_get(url:str, *):
+def api_get(url:str, *)->response:
     return requests.get(
         api_path(url, *),
         headers=bearer_token()
     )
 
-def api_post(url:str, *):
+def api_post(url:str, *)->response:
     return requests.post(
         api_path(url, *),
         headers=bearer_token()
