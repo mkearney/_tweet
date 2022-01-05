cbadbe2edb461e377232c7138fecd4994b26baaf
Michael W. Kearney <mike.wayne.kearney@gmail.com>
2022-01-05 15:08:05 -0600

    Update __init__.py

--- python/pytweet/requests/__init__.py
+++ python/pytweet/requests/__init__.py
@@ -1,1 +1,7 @@
 
+def api_request(endpoint: str, params:List=None)->str:
+    args = []
+    for arg, val in enumerate(params):
+        args.append(f"{arg}={val}")
+    args = "&".join(args)
+    return f"{_base_url_}{endpoint}{args}"
