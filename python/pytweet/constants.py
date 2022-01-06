6f575123ea52ceb55698c954deccb71caf650773
Michael W. Kearney <mike.wayne.kearney@gmail.com>
2022-01-05 23:52:51 -0600

    Update constants.py

--- python/pytweet/constants.py
+++ python/pytweet/constants.py
@@ -4,5 +4,6 @@ __domain__ = "api.twitter.com"
 __version__ = "/1.1"
 __base_url__ = f"{__scheme__}://{__domain__}{__version__}/"
 
-__oauth_token__ = "oauth2/token.json"
-__search_tweets__ = "search/tweets.json"
+__oauth2_token__ = "oauth2/token.json"
+__tweets_search__ = "search/tweets.json"
+__tweets_lookup__ = "tweets/lookup.json"
